06dd5788e8669f371928165e65a7feea353da8df
miits <mikolaj.szal@gmail.com>
2019-03-28 17:58:11 +0100

    added test datasets, updated NeighbourhoodAnalyzer tests

--- src/test/java/org/ordinalclassification/utils/NeighbourhoodAnalyzerTest.java
+++ src/test/java/org/ordinalclassification/utils/NeighbourhoodAnalyzerTest.java
@@ -1,6 +1,5 @@
 package org.ordinalclassification.utils;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.ordinalclassification.types.AnalysisResult;
 import org.ordinalclassification.types.LearningExampleType;
@@ -29,8 +28,27 @@ class NeighbourhoodAnalyzerTest {
                 safeCounter++;
             }
         }
-        double resultsPercent = safeCounter / len * 100;
+        double resultsPercent = (double) safeCounter / len * 100;
         double expectedPercent = 100;
+        assertEquals(expectedPercent, resultsPercent);
+
+        setName = "haberman-S100";
+        jsonPath = String.format("data\\test\\json\\%s.json", setName);
+        csvPath = String.format("data\\test\\csv\\%s.csv", setName);
+        analyzer = new NeighbourhoodAnalyzer(jsonPath, csvPath, "");
+        analyzer.runAnalysisSilent();
+        resultsByName = analyzer.getResultsByName();
+        results = resultsByName.get("union_vs_union_knn");
+        perExampleResults = results.getPerExampleResults();
+        safeCounter = 0;
+        len = perExampleResults.size();
+        for (ResultRow res: perExampleResults) {
+            if (res.getType() == String.valueOf(LearningExampleType.SAFE)) {
+                safeCounter++;
+            }
+        }
+        resultsPercent = (double) safeCounter / len * 100;
+        expectedPercent = 100;
         assertEquals(expectedPercent, resultsPercent);
     }
 
@@ -62,6 +80,54 @@ class NeighbourhoodAnalyzerTest {
         assertEquals(expectedBorderPercent, borderPercent);
     }
 
+    @Test
+    void shouldReturnAllOutlier() {
+        String setName = "car-O100";
+        String jsonPath = String.format("data\\test\\json\\%s.json", setName);
+        String csvPath = String.format("data\\test\\csv\\%s.csv", setName);
+        NeighbourhoodAnalyzer analyzer = new NeighbourhoodAnalyzer(jsonPath, csvPath, "");
+        analyzer.runAnalysisSilent();
+        HashMap<String, AnalysisResult> resultsByName = analyzer.getResultsByName();
+        AnalysisResult results = resultsByName.get("union_vs_union_knn");
+        List<ResultRow> perExampleResults = results.getPerExampleResults();
+        int outlierCounter = 0;
+        int len = perExampleResults.size();
+        for (ResultRow res: perExampleResults) {
+            if (res.getType() == String.valueOf(LearningExampleType.OUTLIER)) {
+                outlierCounter++;
+            }
+        }
+        double resultsPercent = (double) outlierCounter / len * 100;
+        double expectedPercent = 100;
+        assertEquals(expectedPercent, resultsPercent);
+    }
+
+    @Test
+    void shouldReturn50Borderline50Outlier() {
+        String setName = "car-B50-O50";
+        String jsonPath = String.format("data\\test\\json\\%s.json", setName);
+        String csvPath = String.format("data\\test\\csv\\%s.csv", setName);
+        NeighbourhoodAnalyzer analyzer = new NeighbourhoodAnalyzer(jsonPath, csvPath, "");
+        analyzer.runAnalysisSilent();
+        HashMap<String, AnalysisResult> resultsByName = analyzer.getResultsByName();
+        AnalysisResult results = resultsByName.get("union_vs_union_knn");
+        List<ResultRow> perExampleResults = results.getPerExampleResults();
+        int outlierCounter = 0;
+        int borderCounter = 0;
+        double len = perExampleResults.size();
+        for (ResultRow res: perExampleResults) {
+            if (res.getType().equals(String.valueOf(LearningExampleType.OUTLIER))) {
+                outlierCounter++;
+            } else if (res.getType().equals(String.valueOf(LearningExampleType.BORDERLINE))) {
+                borderCounter++;
+            }
+        }
+        double outlierPercent = (double) outlierCounter / len * 100.0;
+        double borderPercent = (double) borderCounter / len * 100.0;
+        double expectedPercent = 50;
+        assertEquals(expectedPercent, outlierPercent);
+        assertEquals(expectedPercent, borderPercent);
+    }
 
     @Test
     void shouldReturnEquallyDistributed() {
@@ -94,6 +160,39 @@ class NeighbourhoodAnalyzerTest {
         double rarePercent = (double) rareCounter / len * 100.0;
         double outlierPercent = (double) outlierCounter / len * 100.0;
         double expectedPercent = 25;
+        assertEquals(expectedPercent, safePercent);
+        assertEquals(expectedPercent, borderPercent);
+        assertEquals(expectedPercent, rarePercent);
+        assertEquals(expectedPercent, outlierPercent);
+
+        setName = "haberman-B25-O25-R25-S25";
+        jsonPath = String.format("data\\test\\json\\%s.json", setName);
+        csvPath = String.format("data\\test\\csv\\%s.csv", setName);
+        analyzer = new NeighbourhoodAnalyzer(jsonPath, csvPath, "");
+        analyzer.runAnalysisSilent();
+        resultsByName = analyzer.getResultsByName();
+        results = resultsByName.get("union_vs_union_knn");
+        perExampleResults = results.getPerExampleResults();
+        safeCounter = 0;
+        borderCounter = 0;
+        rareCounter = 0;
+        outlierCounter = 0;
+        len = perExampleResults.size();
+        for (ResultRow res: perExampleResults) {
+            if (res.getType().equals(String.valueOf(LearningExampleType.SAFE))) {
+                safeCounter++;
+            } else if (res.getType().equals(String.valueOf(LearningExampleType.BORDERLINE))) {
+                borderCounter++;
+            } else if (res.getType().equals(String.valueOf(LearningExampleType.RARE))) {
+                rareCounter++;
+            } else if (res.getType().equals(String.valueOf(LearningExampleType.OUTLIER))) {
+                outlierCounter++;
+            }
+        }
+        safePercent = (double) safeCounter / len * 100.0;
+        borderPercent = (double) borderCounter / len * 100.0;
+        rarePercent = (double) rareCounter / len * 100.0;
+        outlierPercent = (double) outlierCounter / len * 100.0;
         assertEquals(expectedPercent, safePercent);
         assertEquals(expectedPercent, borderPercent);
         assertEquals(expectedPercent, rarePercent);
